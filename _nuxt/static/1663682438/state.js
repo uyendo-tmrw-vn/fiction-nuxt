bc25c9af38884d637dea4ce97d67989517dd7d13
uyendo-tmrw-vn <uyendo-tmrw-vn@users.noreply.github.com>
2022-09-20 14:00:45 +0000

    deploy: e02852522d09478eca56dcbaff532adaffade5d1

---
+++ _nuxt/static/1663682438/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663682438",layout:"main",error:a,state:{articlesSingle:{list:["Lorem ipsum","dolor sit amet","consetetur sadipscing elitr"]},todos:{todoList:[]}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
