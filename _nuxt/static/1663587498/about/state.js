f509aa30ce90644aef796a6bbadd92b3bb36b9bc
Uyen Do TMRW-VN <99628467+uyendo-tmrw-vn@users.noreply.github.com>
2022-09-20 20:54:00 +0700

    05c1cde

---
+++ _nuxt/static/1663587498/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663587498",layout:"main",error:a,state:{articlesSingle:{list:["Lorem ipsum","dolor sit amet","consetetur sadipscing elitr"]},todos:{todoList:[]}},serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
