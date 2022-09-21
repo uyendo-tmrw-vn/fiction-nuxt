f772ed6d127aa254c14ffb2fd277b699bb384a11
uyendo-tmrw-vn <uyendo-tmrw-vn@users.noreply.github.com>
2022-09-21 06:54:41 +0000

    deploy: c84b1cf710a876a920097d7053073f3742df9f8c

--- _nuxt/045e0c2.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{359:function(t,n,e){"use strict";e.r(n);var l={layout:"blog"},o=e(36),component=Object(o.a)(l,(function(){return(0,this._self._c)("h1",{staticClass:"title mb-4 border text-center"},[this._v("\n\tPage Blog\n")])}),[],!1,null,null,null);n.default=component.exports}}]);
